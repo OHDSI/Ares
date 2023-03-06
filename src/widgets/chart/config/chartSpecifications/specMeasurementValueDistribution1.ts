dd3e37a8d2449a3dbbb59e898f24715bf5842eaf
Mikhail Iontsev <mikhail_iontsev@icloud.com>
2023-03-07 01:24:04 +0700

    feat: updated tooltips for distribution reports to include more data

--- src/widgets/chart/config/chartSpecifications/specMeasurementValueDistribution1.ts
+++ src/widgets/chart/config/chartSpecifications/specMeasurementValueDistribution1.ts
@@ -35,6 +35,16 @@ export function specMeasurementValueDistribution1(
       {
         mark: { type: "bar", size: 14, tooltip: true },
         encoding: {
+          tooltip: [
+            { field: "CATEGORY", title: "CATEGORY" },
+            { field: "MIN_VALUE", title: "MIN_VALUE" },
+            { field: "P10_VALUE", title: "P10_VALUE" },
+            { field: "P25_VALUE", title: "P25_VALUE" },
+            { field: "MEDIAN_VALUE", title: "MEDIAN_VALUE" },
+            { field: "P75_VALUE", title: "P75_VALUE" },
+            { field: "P90_VALUE", title: "P90_VALUE" },
+            { field: "MAX_VALUE", title: "MAX_VALUE" },
+          ],
           x: { field: "P25_VALUE", type: "quantitative" },
           x2: { field: "P75_VALUE" },
           color: {
