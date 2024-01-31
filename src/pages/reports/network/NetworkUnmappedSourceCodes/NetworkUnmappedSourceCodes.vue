91928cf41b5b786afecbc2eff90eafd35eb445ed
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2024-01-31 08:33:58 +0500

    feat: add pivot table to unmapped source codes

--- src/pages/reports/network/NetworkUnmappedSourceCodes/NetworkUnmappedSourceCodes.vue
+++ src/pages/reports/network/NetworkUnmappedSourceCodes/NetworkUnmappedSourceCodes.vue
@@ -1,63 +1,81 @@
 <template>
-  <Panel
-    v-if="store.getters.dataInStore"
-    header="Network Unmapped Source Codes"
-  >
-    <DataTable
-      removable-sort
-      size="small"
-      :globalFilterFields="['CDM_TABLE_NAME', 'CDM_FIELD_NAME']"
-      paginator
-      v-model:filters="newFilters"
-      :value="store.getters.getData.domainTable"
-      :rows="10"
-      :rowsPerPageOptions="[5, 10, 20, 50]"
+  <div class="flex flex-col gap-5">
+    <Panel
+      v-if="store.getters.dataInStore"
+      header="Network Unmapped Source Codes"
     >
-      <template #header>
-        <div>
-          <InputGroup unstyled>
-            <InputGroupAddon>
-              <i class="pi pi-search"></i>
-            </InputGroupAddon>
-            <InputText
-              class="rounded-r-lg"
-              style="width: 45rem"
-              unstyled
-              v-model="newFilters['global'].value"
-              placeholder="Search in Table"
-            />
-          </InputGroup>
-        </div>
-      </template>
-      <Column sortable header="CDM Table" field="CDM_TABLE_NAME"> </Column>
-      <Column sortable header="CDM Field" field="CDM_FIELD_NAME"> </Column>
-      <Column sortable header="Source Value" field="SOURCE_VALUE"> </Column>
-      <Column
-        sortable
-        header="# Data Sources"
-        field="DATA_SOURCE_COUNT"
-      ></Column>
-      <Column sortable header="Data Sources" field="DATA_SOURCES"></Column>
-      <Column
-        :pt="{ headerContent: 'justify-end' }"
-        sortable
-        header="# Records"
-        field="RECORD_COUNT"
+      <DataTable
+        removable-sort
+        size="small"
+        :globalFilterFields="['CDM_TABLE_NAME', 'CDM_FIELD_NAME']"
+        paginator
+        v-model:filters="newFilters"
+        :value="store.getters.getData.domainTable"
+        :rows="10"
+        :rowsPerPageOptions="[5, 10, 20, 50]"
       >
-        <template #body="slotProps">
-          <div class="flex justify-end">
-            <span>
-              {{
-                slotProps.data.RECORD_COUNT
-                  ? helpers.formatComma(slotProps.data.RECORD_COUNT)
-                  : "No data"
-              }}</span
-            >
+        <template #header>
+          <div>
+            <InputGroup unstyled>
+              <InputGroupAddon>
+                <i class="pi pi-search"></i>
+              </InputGroupAddon>
+              <InputText
+                class="rounded-r-lg"
+                style="width: 45rem"
+                unstyled
+                v-model="newFilters['global'].value"
+                placeholder="Search in Table"
+              />
+            </InputGroup>
           </div>
         </template>
-      </Column>
-    </DataTable>
-  </Panel>
+        <Column sortable header="CDM Table" field="CDM_TABLE_NAME"> </Column>
+        <Column sortable header="CDM Field" field="CDM_FIELD_NAME"> </Column>
+        <Column sortable header="Source Value" field="SOURCE_VALUE"> </Column>
+        <Column
+          sortable
+          header="# Data Sources"
+          field="DATA_SOURCE_COUNT"
+        ></Column>
+        <Column sortable header="Data Sources" field="DATA_SOURCES"></Column>
+        <Column
+          :pt="{ headerContent: 'justify-end' }"
+          sortable
+          header="# Records"
+          field="RECORD_COUNT"
+        >
+          <template #body="slotProps">
+            <div class="flex justify-end">
+              <span>
+                {{
+                  slotProps.data.RECORD_COUNT
+                    ? helpers.formatComma(slotProps.data.RECORD_COUNT)
+                    : "No data"
+                }}</span
+              >
+            </div>
+          </template>
+        </Column>
+      </DataTable>
+    </Panel>
+
+    <Panel v-if="store.getters.dataInStore" header="Pivot Table">
+      <Pivot
+        :data="store.getters.getData.domainTable"
+        :attributes="[
+          'CDM_TABLE_NAME',
+          'CDM_FIELD_NAME',
+          'SOURCE_VALUE',
+          'RECORD_COUNT',
+          'DATA_SOURCES',
+          'DATA_SOURCE_COUNT',
+        ]"
+        :aggregate-attrs="['RECORD_COUNT', 'DATA_SOURCE_COUNT']"
+        :aggregator-names-list="['Count', 'Sum']"
+      />
+    </Panel>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -71,6 +89,7 @@ import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import { FilterMatchMode } from "primevue/api";
 import { helpers } from "@/shared/lib/mixins";
+import Pivot from "@/widgets/pivot/ui/Pivot.vue";
 
 const store = useStore();
 
