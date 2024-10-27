fcc532cd34caecc579e86e425c97c7719835c203
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2024-10-27 19:22:58 +0400

    improvement: network concept dashboard filters

--- src/pages/reports/network/NetworkConceptDashboard/NetworkConceptDashboard.vue
+++ src/pages/reports/network/NetworkConceptDashboard/NetworkConceptDashboard.vue
@@ -11,10 +11,12 @@
       <DataTable
         removable-sort
         rowGroupMode="subheader"
-        groupRowsBy="CONCEPT_NAME"
         class="table self-start"
+        v-model:filters="filters"
+        :global-filter-fields="['CDM_NAME', 'CONCEPT_NAME', 'CONCEPT_ID']"
         v-if="concepts.length && !loading"
         size="small"
+        filterDisplay="row"
         paginator
         currentPageReportTemplate="{first} to {last} of {totalRecords}"
         paginator-template="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown CurrentPageReport"
@@ -22,11 +24,90 @@
         :rows="10"
         :rowsPerPageOptions="[5, 10, 20, 50]"
       >
-        <Column sortable header="Source" field="CDM_NAME"></Column>
-        <Column sortable header="Concept Name" field="CONCEPT_NAME"></Column>
+        <template #header>
+          <div class="flex flex-row gap-10">
+            <InputGroup unstyled>
+              <InputGroupAddon>
+                <i class="pi pi-search"></i>
+              </InputGroupAddon>
+              <InputText
+                class="rounded-r-lg"
+                style="width: 45rem"
+                unstyled
+                v-model="filters.global.value"
+                placeholder="Search in Table"
+              />
+            </InputGroup>
+            <MultiSelect
+              :pt="{
+                root: 'dark:bg-primary-400 bg-primary-500 text-white rounded',
+                labelContainer:
+                  'dark:bg-primary-400 bg-primary-500 text-white rounded',
+                trigger: [
+                  'flex items-center justify-center shrink-0 rounded-tr-md rounded-br-md dark:bg-primary-400 w-12',
+                ],
+              }"
+              data-key="title"
+              option-label="title"
+              option-value="key"
+              :options="availableHeaders"
+              placeholder="Select Columns"
+              v-model="selectedColumns"
+            >
+              <template #value>
+                <span class="flex flex-row w-full text-white items-center">
+                  <svg-icon type="mdi" :path="mdiTable"></svg-icon>
+                  <span class="uppercase text-base">Columns to display</span>
+                </span>
+              </template>
+              <template #dropdownicon>
+                <span></span>
+              </template>
+            </MultiSelect>
+          </div>
+        </template>
 
-        <Column sortable header="Concept ID" field="CONCEPT_ID"></Column>
         <Column
+          v-if="selectedColumns.includes('CDM_NAME')"
+          unstyled
+          sortable
+          filter-field="CDM_NAME"
+          :show-filter-menu="false"
+          :show-clear-button="false"
+          field="CDM_NAME"
+          header="Source"
+        >
+          <template #body="{ data }">
+            {{ data.CDM_NAME }}
+          </template>
+          <template #filter="{ filterModel, filterCallback }">
+            <MultiSelect
+              :maxSelectedLabels="2"
+              show-clear
+              filter
+              v-model="filterModel.value"
+              @change="filterCallback()"
+              :options="statusOptions"
+              placeholder="Select Source"
+              class="p-column-filter w-full"
+              style="min-width: 12rem"
+            ></MultiSelect>
+          </template>
+        </Column>
+        <Column
+          v-if="selectedColumns.includes('CONCEPT_NAME')"
+          sortable
+          header="Concept Name"
+          field="CONCEPT_NAME"
+        ></Column>
+        <Column
+          v-if="selectedColumns.includes('CONCEPT_ID')"
+          sortable
+          header="Concept ID"
+          field="CONCEPT_ID"
+        ></Column>
+        <Column
+          v-if="selectedColumns.includes('PEOPLE_COUNT')"
           sortable
           header="# People"
           field="PEOPLE_COUNT"
@@ -38,6 +119,7 @@
           </template>
         </Column>
         <Column
+          v-if="selectedColumns.includes('PEOPLE_PERCENT')"
           sortable
           header="% People"
           field="PEOPLE_PERCENT"
@@ -75,7 +157,7 @@
 import { ConceptSearchForm } from "@/widgets/conceptSearchForm";
 import "./index.css";
 
-import { computed, Ref, ref } from "vue";
+import { computed, onBeforeMount, Ref, ref } from "vue";
 import { useStore } from "vuex";
 import Panel from "primevue/panel";
 import Button from "primevue/button";
@@ -84,7 +166,15 @@ import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import { webApiActions } from "@/shared/api/webAPI";
 import ProgressCircle from "@/entities/ProgressCircle.vue";
+import MultiSelect from "primevue/multiselect";
+import { FilterMatchMode } from "primevue/api";
+import { mdiTable } from "@mdi/js";
+import InputGroup from "primevue/inputgroup";
+import InputGroupAddon from "primevue/inputgroupaddon";
+import InputText from "primevue/inputtext";
 const store = useStore();
+
+const selectedColumns = ref([]);
 
 const successMessage: Ref<string[]> = ref([]);
 const errors: Ref<string> = ref("");
@@ -99,6 +189,51 @@ const handleFailedLoading = function () {
   loading.value = false;
 };
 
+const statusOptions = computed(() => {
+  return helpers.getValuesArray(concepts.value, "CDM_NAME", true);
+});
+
+const headers = ref({
+  CDM_NAME: {
+    title: "Source",
+    sortable: true,
+    key: "CDM_NAME",
+    show: true,
+    default: true,
+  },
+  CONCEPT_NAME: {
+    title: "Concept Name",
+    sortable: true,
+    key: "CONCEPT_NAME",
+    show: true,
+    default: true,
+  },
+  CONCEPT_ID: {
+    title: "Concept ID",
+    sortable: true,
+    key: "CONCEPT_ID",
+    show: false,
+    default: false,
+  },
+  PEOPLE_COUNT: {
+    title: "# People",
+    sortable: true,
+    key: "PEOPLE_COUNT",
+    show: true,
+    default: true,
+  },
+  PEOPLE_PERCENT: {
+    title: "% People",
+    sortable: true,
+    key: "PEOPLE_PERCENT",
+    show: true,
+    default: true,
+  },
+});
+const availableHeaders = computed(() => {
+  return Object.values(headers.value);
+});
+
 const save = function () {
   const newConcept = store.getters.getData?.concept;
   concepts.value = [...concepts.value, ...newConcept];
@@ -111,6 +246,11 @@ const clearMessages = function () {
   errors.value = "";
   successMessage.value = [];
 };
+
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  CDM_NAME: { value: null, matchMode: FilterMatchMode.IN },
+});
 const close = function (event) {
   if (event.loading) {
     loading.value = true;
@@ -119,6 +259,12 @@ const close = function (event) {
   store.dispatch(webApiActions.RESET_API_STORAGE);
   successMessage.value = [];
 };
+
+onBeforeMount(() => {
+  selectedColumns.value = Object.values(headers.value)
+    .filter((col) => col.default && col.show)
+    .map((col) => col.key);
+});
 </script>
 <script lang="ts">
 export default {
