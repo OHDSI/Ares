71251d37576fddd3a29563dd7ce684c4e1ac4997
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2024-12-16 01:46:34 +0500

    improvement: flexible form width

--- src/widgets/conceptSearchForm/ui/ConceptSearchForm.vue
+++ src/widgets/conceptSearchForm/ui/ConceptSearchForm.vue
@@ -2,7 +2,7 @@
   <Dialog
     v-if="webapiEnabled && authenticated"
     :pt="{
-      root: { class: 'w-[1000px]' },
+      root: { class: 'min-w-[1000px] max-w-[1500px]' },
     }"
     header="Vocabulary Search"
     unstyled
