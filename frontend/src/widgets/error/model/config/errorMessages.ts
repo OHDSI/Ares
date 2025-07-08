5a11d6b4e668fe27d708685c7642d4e8c7dabf32
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2025-07-09 03:26:44 +0700

    improvement: new missing cost files message

--- frontend/src/widgets/error/model/config/errorMessages.ts
+++ frontend/src/widgets/error/model/config/errorMessages.ts
@@ -29,8 +29,8 @@ export default {
     index: "Index file is missing. Please run AresIndexer then try again",
     location:
       "Location data is not available. Please run the latest version of achilles then try again",
-    costDrilldown: "Cost drilldown report files are not found",
-    costTable: "Cost table files are not found",
+    costDrilldown: "Cost information not available for this data source.",
+    costTable: "Cost information not available for this data source",
   },
   technicalError: {
     codeError:
