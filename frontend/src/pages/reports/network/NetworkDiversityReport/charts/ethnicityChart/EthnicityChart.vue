6eaad0b4e02aa45a1bcfbdd7bddb5e6d6290623c
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2025-07-20 07:00:39 +0700

    improvement: network diversity report dynamic height

--- frontend/src/pages/reports/network/NetworkDiversityReport/charts/ethnicityChart/EthnicityChart.vue
+++ frontend/src/pages/reports/network/NetworkDiversityReport/charts/ethnicityChart/EthnicityChart.vue
@@ -2,8 +2,9 @@
   <Panel header="Ethnicity">
     <Echarts
       id="viz-network-ethnicity"
-      :data="filteredData"
+      :data="data"
       :chart-spec="getEChartsEthnicity"
+      :height="totalHeight"
     />
   </Panel>
 </template>
@@ -11,26 +12,24 @@
 <script setup lang="ts">
 import Panel from "primevue/panel";
 import { useStore } from "vuex";
-import { computed, ref } from "vue";
+import { computed } from "vue";
 import Echarts from "@/widgets/echarts/Echarts.vue";
 import getEChartsEthnicity from "@/pages/reports/network/NetworkDiversityReport/charts/ethnicityChart/ethnicity";
+import { helpers } from "@/shared/lib/mixins";
 
 const store = useStore();
-const selectedFilters = ref([]);
 
 const data = computed(() => {
   return store.getters.getData.ethnicityData;
 });
 
-const filteredData = computed(() => {
-  if (selectedFilters.value.length) {
-    return data.value.filter((val) =>
-      selectedFilters.value.includes(val.CONCEPT_NAME)
-    );
-  } else {
-    return data.value;
-  }
-});
+const trellis = helpers.getValuesArray(data.value, "DATA_SOURCE_KEY", true);
+
+const facetCount = trellis.length;
+const perFacetHeight = 50;
+const minHeight = 200;
+
+const totalHeight = `${Math.max(facetCount * perFacetHeight, minHeight)}px`;
 </script>
 
 <style scoped></style>
--- frontend/src/pages/reports/network/NetworkDiversityReport/charts/raceChart/RaceChart.vue
+++ frontend/src/pages/reports/network/NetworkDiversityReport/charts/raceChart/RaceChart.vue
@@ -1,33 +1,35 @@
 <template>
   <Panel header="Race">
-    <Echarts id="viz-race" :data="filteredData" :chart-spec="getEChartsRace" />
+    <Echarts
+      id="viz-race"
+      :data="data"
+      :chart-spec="getEChartsRace"
+      :height="totalHeight"
+    />
   </Panel>
 </template>
 
 <script setup lang="ts">
 import Panel from "primevue/panel";
 import { useStore } from "vuex";
-import { computed, ref } from "vue";
+import { computed } from "vue";
 import Echarts from "@/widgets/echarts/Echarts.vue";
 import getEChartsRace from "@/pages/reports/network/NetworkDiversityReport/charts/raceChart/race";
+import { helpers } from "@/shared/lib/mixins";
 
 const store = useStore();
-
-const selectedFilters = ref([]);
 
 const data = computed(() => {
   return store.getters.getData.raceData;
 });
 
-const filteredData = computed(() => {
-  if (selectedFilters.value.length) {
-    return data.value.filter((val) =>
-      selectedFilters.value.includes(val.CONCEPT_NAME)
-    );
-  } else {
-    return data.value;
-  }
-});
+const trellis = helpers.getValuesArray(data.value, "DATA_SOURCE_KEY", true);
+
+const facetCount = trellis.length;
+const perFacetHeight = 50;
+const minHeight = 200;
+
+const totalHeight = `${Math.max(facetCount * perFacetHeight, minHeight)}px`;
 </script>
 
 <style scoped></style>
