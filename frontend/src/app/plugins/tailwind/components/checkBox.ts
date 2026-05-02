6391c665eeffd10db0ea4117e4256dd8e991e2fd
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2026-05-02 21:03:30 +0700

    improvement: checkbox styles

--- frontend/src/app/plugins/tailwind/components/checkBox.ts
+++ frontend/src/app/plugins/tailwind/components/checkBox.ts
@@ -33,22 +33,22 @@ export default {
 
       // Colors
       {
-        "border-surface-200 bg-surface-0 dark:border-surface-700 dark:bg-surface-900":
+        "border-[var(--color-border)] bg-[var(--color-bg-surface)]":
           !context.checked && !props.invalid,
         "border-primary-500 bg-primary-500 dark:border-primary-400 dark:bg-primary-400":
           context.checked,
       },
 
       // Invalid State
-      { "border-red-500 dark:border-red-400": props.invalid },
+      { "border-red-500": props.invalid },
 
       // States
       {
-        "peer-hover:border-primary-500 dark:peer-hover:border-primary-400":
+        "peer-hover:border-primary-500":
           !props.disabled && !context.checked && !props.invalid,
-        "peer-hover:bg-primary-600 dark:peer-hover:bg-primary-300 peer-hover:border-primary-700 dark:peer-hover:border-primary-300":
+        "peer-hover:bg-primary-600 peer-hover:border-primary-700":
           !props.disabled && context.checked,
-        "peer-focus-visible:border-primary-500 dark:peer-focus-visible:border-primary-400 peer-focus-visible:ring-2 peer-focus-visible:ring-primary-400/20 dark:peer-focus-visible:ring-primary-300/20":
+        "peer-focus-visible:border-primary-500 peer-focus-visible:ring-2 peer-focus-visible:ring-primary-400/20":
           !props.disabled,
         "cursor-default opacity-60": props.disabled,
       },
@@ -79,7 +79,7 @@ export default {
       "opacity-0",
       "rounded-md",
       "outline-none",
-      "border-2 border-surface-200 dark:border-surface-700",
+      "border-2 border-[var(--color-border)]",
 
       // Misc
       "appearance-none",
@@ -96,7 +96,7 @@ export default {
       "h-4",
 
       // Colors
-      "text-white dark:text-surface-900",
+      "text-white",
 
       // Transitions
       "transition-all",
